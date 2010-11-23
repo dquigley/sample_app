131f9a1909e52c4e12f4afc52c1f72a99a197f16
David Quigley <david.j.quigley@gmail.com>
2010-11-23 14:19:57 -0600

    Chapter3

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  # Setup page titles
+  def title
+    base_title = "Ruby on Rails Tutorial Sample App"
+    if @title.nil? # if no title is provided then just return the base title
+      base_title
+    else # Otherwise add the title with a pipe and appropriate spaces between each section
+      "#{base_title} | #{@title}"
+    end
+  end
 end
